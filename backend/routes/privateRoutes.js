f78b3847ddeafe94a697104eed14c300d11b5dfa
ImranMallick0808 <imran88mallick@gmail.com>
2024-12-05 22:32:35 +0530

    Update privateRoutes.js

--- backend/routes/privateRoutes.js
+++ backend/routes/privateRoutes.js
@@ -91,7 +91,7 @@ router
 
 
 //gallery
-router.route("/addImage").post(isAuthenticatedUser,addGalleyImage)
+router.route("/addImage").post(singleUpload,addGalleyImage)
 
 router.route("/deleteImage/:id").delete(isAuthenticatedUser,deleteGalleryImage)
 
